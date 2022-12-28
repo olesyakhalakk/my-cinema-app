60536efcbc0099dd60a79e474996f78e5b94aee6
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-12-28 16:37:40 +0200

    Commit

---
+++ src/main/java/cinema/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,29 @@
+package cinema.dao.impl;
+
+import cinema.dao.AbstractDao;
+import cinema.dao.RoleDao;
+import cinema.exception.DataProcessingException;
+import cinema.model.Role;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
+    public RoleDaoImpl(SessionFactory factory) {
+        super(factory, Role.class);
+    }
+
+    @Override
+    public Role getByName(String roleName) {
+        try (Session session = factory.openSession()) {
+            Query<Role> query = session.createQuery(
+                    "FROM Role r WHERE r.roleName = :roleName", Role.class);
+            query.setParameter("roleName", Role.RoleName.valueOf(roleName));
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can not find role with name: " + roleName, e);
+        }
+    }
+}
